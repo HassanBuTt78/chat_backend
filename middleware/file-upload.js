810461dc17ab692f40e3ba98e115f06baddd0377
HassanBuTt78 <hasanalib288@gmail.com>
2024-02-15 18:54:26 +0500

    made upload middleware reusable

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,27 +1,20 @@
 const multer = require("multer");
 const path = require("path");
 const CustomError = require("../utils/custom-error");
-const MAX_FILE_SIZE = 5e7; /*50MB in bytes*/
-const ALLOWED_MIME_TYPES = [
-    "audio/mpeg",
-    "audio/vnd.wav",
-    "audio/mp4",
-    "application/json",
-    "application/pdf",
-    "application/vnd.ms-powerpoint",
-    "text/plain",
-    "application/zip",
-    "image/gif",
-    "image/jpeg",
-    "image/png",
-    "image/webp",
-    "image/avif",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "application/msword",
-    "video/x-msvideo",
-    "video/mpeg",
-    "video/mp4",
-];
+const ALLOWED_MIME_TYPES = {
+    image: ["image/gif", "image/jpeg", "image/png", "image/webp", "image/avif"],
+    audio: ["audio/mpeg", "audio/vnd.wav", "audio/mp4"],
+    document: [
+        "application/json",
+        "application/pdf",
+        "application/vnd.ms-powerpoint",
+        "text/plain",
+        "application/zip",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/msword",
+    ],
+    video: ["video/x-msvideo", "video/mpeg", "video/mp4"],
+};
 
 const storage = multer.diskStorage({
     destination: "public/uploads/",
@@ -37,21 +30,39 @@ const storage = multer.diskStorage({
         );
     },
 });
-const fileFilter = (req, file, cb) => {
-    if (!ALLOWED_MIME_TYPES.includes(file.mimetype)) {
-        return cb(new CustomError(415, "File Type not Allowed"), false);
-    }
-    if (!(req.headers["content-length"] <= MAX_FILE_SIZE)) {
-        return cb(new CustomError(413, "File must be under 50MB"), false);
+
+const uploader = (filetypes, maxSize) => {
+    const allowedFileTypes = [];
+    if (typeof filetypes === "object") {
+        for (const filetype of filetypes) {
+            allowedFileTypes.push(...ALLOWED_MIME_TYPES[filetype]);
+        }
+    } else {
+        allowedFileTypes.push(...ALLOWED_MIME_TYPES[filetypes]);
     }
 
-    cb(null, true);
+    return multer({
+        storage: storage,
+        limits: { fileSize: maxSize * 1000000 },
+        fileFilter: (req, file, cb) => {
+            if (!allowedFileTypes.includes(file.mimetype)) {
+                return cb(
+                    new CustomError(
+                        415,
+                        `Only [${filetypes}] are allowed to upload on this route`
+                    ),
+                    false
+                );
+            }
+            if (!(req.headers["content-length"] <= maxSize * 1000000)) {
+                return cb(
+                    new CustomError(413, `File must be under ${maxSize}MB`),
+                    false
+                );
+            }
+            cb(null, true);
+        },
+    });
 };
 
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: MAX_FILE_SIZE },
-    fileFilter: fileFilter,
-});
-
-module.exports = upload;
+module.exports = uploader;
--- router/uploads.routes.js
+++ router/uploads.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const uploadsController = require("../controller/uploads.controller.js");
 const userAuthorize = require("../middleware/user-authorization.js");
-const upload = require("../middleware/file-upload.js");
+const uploader = require("../middleware/file-upload.js");
 
 router.post(
     "/",
-    [userAuthorize, upload.array("file")],
+    [userAuthorize, uploader("document", 5).array("file")],
     uploadsController.uploadFile
 );
 router.get("/:filename", uploadsController.getUpload);
